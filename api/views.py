30de93cb657f7686a026cf4aa17e6c081aaf0cc9
konekato <18ec033@ms.dendai.ac.jp>
2020-01-21 00:41:55 +0900

    uriで表示

--- api/views.py
+++ api/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpRequest
+from django.views.generic import TemplateView
 
-# Create your views here.
+class SelectQuestions(TemplateView):
+    template_name = "select_questions.html"
