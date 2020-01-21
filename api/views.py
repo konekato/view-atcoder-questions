7427b63262b8150ef7a3964765132ba608491ef5
konekato <18ec033@ms.dendai.ac.jp>
2020-01-21 14:23:17 +0900

    add stylessheet

--- api/views.py
+++ api/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpRequest
 from django.views.generic import TemplateView
 
 class SelectQuestions(TemplateView):
     template_name = "select_questions.html"
+
+class ShowQuestion(TemplateView):
+    template_name = "show_question.html"
--- view_atcoder_questions/urls.py
+++ view_atcoder_questions/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from api.views import SelectQuestions
+import api.views as api
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', SelectQuestions.as_view())
+    path('home/', api.SelectQuestions.as_view()),
+    path('home/question/', api.ShowQuestion.as_view())
 ]
