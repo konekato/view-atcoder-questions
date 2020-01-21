83c58f2569d16d1f27fdeb24562a3189b0ec9c61
konekato <18ec033@ms.dendai.ac.jp>
2020-01-21 19:02:17 +0900

    change views.py

--- api/views.py
+++ api/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+
+
 class SelectQuestions(TemplateView):
     template_name = "select_questions.html"
 
 class ShowQuestion(TemplateView):
     template_name = "show_question.html"
+
+    def show(self, request, *args, **kwargs):
+        context = super(ShowQuestion, self).get_context_data(**kwargs)
+
+        return render(self.request, self.template_name, context)
