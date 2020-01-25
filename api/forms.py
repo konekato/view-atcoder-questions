4ca5b79121bb3bd5b77b40a2a4ab101bafd6a5e6
konekato <18ec033@ms.dendai.ac.jp>
2020-01-26 01:23:40 +0900

    viewでコンテスト番号、問題番号表示できるよう修正

--- api/forms.py
+++ api/forms.py
@@ -1,10 +1,26 @@
 from django import forms
 
 CONTESTS = (
-    {'','Select'},
-    ('ABC','AtCoder Begginer Contest'),
-    ('ARC','AtCoder Regular Contest'),
-    ('AGC','AtCoder Grand Contest'),
+    ('','Select'),
+    ('abc','AtCoder Begginer Contest'),
+    ('arc','AtCoder Regular Contest'),
+    ('agc','AtCoder Grand Contest'),
+)
+
+NUMBERS = [[0 for i in range(2)] for j in range(11)]
+NUMBERS[0] = {'',"Select"}
+for i in range(10):
+    for j in range(2):
+        NUMBERS[i+1][j] = str(i+1)
+
+QUESTIONS = (
+    ('','Select'),
+    ('a','A'),
+    ('b','B'),
+    ('c','C'),
+    ('d','D'),
+    ('e','E'),
+    ('f','F'),
 )
 
 class ViewContests(forms.Form):
@@ -15,3 +31,19 @@ class ViewContests(forms.Form):
         choices=CONTESTS,
         widget=forms.Select,
     )
+    numbers = forms.ChoiceField(
+        label='Numbers',
+        required=True,
+        disabled=False,
+        choices=NUMBERS,
+        widget=forms.Select,
+    )
+    questions = forms.ChoiceField(
+        label='Questions',
+        required=True,
+        disabled=False,
+        choices=QUESTIONS,
+        widget=forms.Select,
+    )
+
+    
