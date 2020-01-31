49af9d9f23d99e71b6eb800bbd1df7e3e29c5ae5
konekato <18ec033@ms.dendai.ac.jp>
2020-01-31 17:09:12 +0900

    get.pyでgetしたのを取得

--- api/get.py
+++ api/get.py
@@ -1,24 +1,24 @@
 import requests
+from django.views.generic import TemplateView
 from bs4 import BeautifulSoup
 
-from forms import *
-FORM_NAME_NUMBER = 0
+from views import ShowQuestion
+get = ShowQuestion.get()
 
-num = 1
-contest=CONTESTS[num][FORM_NAME_NUMBER]
-number = NUMBERS[num][FORM_NAME_NUMBER]
-question = QUESTIONS[num][FORM_NAME_NUMBER]
-# 小さく変数化したいcontest=CONTESTS[1][FORM_NAME_NUMBER] みたいな
+contest = get['contest']
+number = get['number']
+question = get['question']
+
 url = "https://atcoder.jp/contests/" + contest + number + "/tasks/" + contest + number + "_" + question
 
 print(url)
 
-r = requests.get(url)
-soup = BeautifulSoup(r.text, "html.parser")
-# print(soup.prettify())
+# r = requests.get(url)
+# soup = BeautifulSoup(r.text, "html.parser")
+# # print(soup.prettify())
 
-body = soup.find("div", id="task-statement").find("span", class_="lang-ja").find_all("div", class_="part")
+# body = soup.find("div", id="task-statement").find("span", class_="lang-ja").find_all("div", class_="part")
 
-length = len(body)
+# length = len(body)
 
-# print(body)
+# # print(body)
--- api/views.py
+++ api/views.py
@@ -1,18 +1,30 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-from .forms import *
+from forms import *
 
 class SelectQuestions(TemplateView):
     def select_questions(request):
         template_name = "select_questions.html"
-        c = {'form': ViewContests(),}
-        return render(request, template_name, c)
+        context = {'form': ViewContests(),}
+        return render(request, template_name, context)
 
 class ShowQuestion(TemplateView):
-    template_name = "show_question.html"
+    def get():
+        # d = {
+        #     'contest': request.GET.get('contests'),
+        #     'number': request.GET.get('numbers'),
+        #     'question': request.GET.get('questions'),
+        #     }
+        d = {
+            'contest': 'abc',
+            'number': '1',
+            'question': 'A',
+            }
+        return d
+        
+    def show(request):
+        template_name = "show_question.html"
+        context = get()
 
-    def show(self, request, *args, **kwargs):
-        context = super(ShowQuestion, self).get_context_data(**kwargs)
-
-        return render(self.request, self.template_name, context)
+        return render(request, template_name, context)
