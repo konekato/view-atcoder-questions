d1ac8948342027f0b3036e0ed36f370cf02d26e2
konekato <18ec033@ms.dendai.ac.jp>
2020-02-01 13:41:45 +0900

    問題を出力できるようにした

--- api/views.py
+++ api/views.py
@@ -13,13 +13,11 @@ class SelectQuestions(TemplateView):
         return render(request, template_name, context)
 
 class ShowQuestion(TemplateView):
-    
-
     def show(request):
         template_name = "show_question.html"
 
-        # scraping
         def scraping(request):
+            # post data
             d = {
                 'contest': request.POST.get('contests'),
                 'number': request.POST.get('numbers'),
@@ -29,23 +27,39 @@ class ShowQuestion(TemplateView):
             number = d['number']
             question = d['question']
 
+            # scraping
             url = "https://atcoder.jp/contests/" + contest + number + "/tasks/" + contest + number + "_" + question
-            print(url)
-            return d
+            res = requests.get(url).text
+            soup = BeautifulSoup(res, "html.parser")
+            h = soup.find("div", id="task-statement").find("span", class_="lang-ja").find_all("div", class_="part")
+            length = len(h)
+            html = []
+            for i in range(length):
+                html.append(h[i].text)
 
-        # def get(request):
-        #     d = {
-        #         'contest': request.POST.get('contests'),
-        #         'number': request.POST.get('numbers'),
-        #         'question': request.POST.get('questions'),
-        #         }
-        #     # d = {
-        #     #     'contest': 'abc',
-        #     #     'number': '1',
-        #     #     'question': 'A',
-        #     #     }
-        #     return d
+            # assign html to d
+            oddn = even = 1
+            for i in range(length):
+                if i == 0:
+                    d['statement'] = html[i]
+                elif i == 1:
+                    d['constraint'] = html[i]
+                elif i == 2:
+                    d['input'] = html[i]
+                elif i == 3:
+                    d['output'] = html[i]
+                else:
+                    if i % 2 == 0:
+                        d['insample'+str(oddn)] = html[i]
+                        oddn += 1
+                    else:
+                        d['outsample'+str(even)] = html[i]
+                        even += 1
+
+            return d
         
         context = scraping(request)
+
+        print(context)
         
         return render(request, template_name, context)
